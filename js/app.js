6e63e0146ba168edaee220a861f964a621d0bec7
John Unruh <j_unruh@hotmail.com>
2015-05-23 07:49:46 -0600

    mark as * and delete working

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var newEmail = function(index, subject) {
       <div class="col-sm-1 email-subject">\
         <p>\
           <input type="checkbox" name="message-' + index + '">\
-          <i class="fa fa-star-o fa-lg"></i>\
+          <i class="star fa fa-star-o fa-lg"></i>\
         </p>\
       </div>\
     <div class="col-sm-11">\
@@ -15,6 +15,7 @@ var newEmail = function(index, subject) {
   return emailHtml;
 }
 
+// TODO - add click handlers to star and checkbox
 
 var addEmail = function(index, subject) { // adds email at end of the email reader section
   $(".first-row").append(newEmail(index, subject));
@@ -26,36 +27,47 @@ var addEmails = function(emailArray) { // add array of email objects
   })
 }
 
-var setRead = function(email) {
-  email.wrapInner("<strong></strong");
-  email.removeClass("unread");
+var setRead = function(index, email) {
+  if(email.hasClass("unread")) {
+    email.wrapInner("<strong></strong");
+    email.removeClass("unread");
+  }
 }
 
-var processCheckedEmail = function(setFunc) { // find checked email and run setFunc ov each
-  $('.email-row input [type="checkbox"]').attr("checked").each(setFunc);
+// find checked email and run setFunc ov each
+// setFunc must take 2 args - index of item and element
+var processCheckedEmail = function(setFunc) {
+  $('.email-row :checked').closest(".email-row").each(setFunc);
 }
 
 var markAsRead = function() {
-  processCheckedEmail(setRead());
+  // must save initial selection as second line won't match aftre .unread is removed
+  var selectedUnread = $('.email-row.unread.selected');
+  selectedUnread.addClass("read");
+  selectedUnread.removeClass("unread");
 }
 
 var markAsUnRead = function() {
-  processCheckedEmail(setUnRead());
+  // must save initial selection as second line won't match aftre .unread is added
+  var selectedRead = $('.email-row.selected').not(".unread");
+  selectedRead.addClass("unread");
+  selectedRead.removeClass("read");
 }
 
-var deleteEmail = function(el) {
-  $('.email-row input [type="checkbox"]').attr("checked").parentsUntil(".email-row").parent().remove();
+var deleteEmail = function() {
+  alert("in delete email")
+  $('.email-row :checked').closest(".email-row").remove();
 }
 
 
-var setUnRead = function(email) {
-  email.unwrop();
-  email.addClass("unread");
+var setUnRead = function(index, email) {
+  if(! email.hasclass("unread")) {
+    email.unwrop();
+    email.addClass("unread");
+  }
 }
 
-var removeCheckedEmails = function() {
-  $(".first-row input :checked").parentsUntil(".first-row").remove();
-}
+
 var toggleStar = function(el) {
   if (el.hasClass("fa-star-o")) {
     el.removeClass("fa-star-o");
@@ -84,56 +96,73 @@ var removeLabel = function(email, labelText) {
   processCheckedEmail(function(el) {removeLabelFromEmail(labelText, el)});
 }
 
-var selectEmail = function(email) {
-  email.addClass("selected");
-  email.find(":checkbox").addAttr("checked", true);
+var setTopBarState = function() {
+  setButtonState();
+  setMultiSelectIcon();
 }
 
 var selectAllEmails = function() {
-  $('.email-row :checkbox').attr("checked", true);
+  $('.email-row :checkbox').prop("checked", true);
   $('.email-row').addClass("selected");
+  setTopBarState();
 }
 
 var unSelectAllEmails = function() {
-  $('.email-row :checkbox').removeAttr("checked");
+  $('.email-row :checkbox').prop("checked", false);
   $('.email-row').removeClass("selected");
+  setTopBarState();
+}
+
+var multiSelectIcon = function() { // return correct icon value for multiSelect
+    var numSelected = $('.email-row :checked').length;
+    if (numSelected === 0)
+      return "fa-square-o";
+    var numCheckboxes = $('.email-row :checkbox').length;
+    if (numCheckboxes === numSelected)
+      return "fa-check-square-o";
+    return "fa-minus-square-o";
+}
+
+var setMultiSelectIcon = function() {
+  var msi = $(".multiSelect i");
+  $(msi).removeClass("fa-minus-square-o");
+  $(msi).removeClass("fa-check-square-o");
+  $(msi).removeClass("fa-square-o");
+  $(msi).addClass(multiSelectIcon());
 }
 
 var multiSelectStateChange = function() {
-  alert("in multiselect state change")
   var icon = $(this).find("i");
   if (icon.hasClass("fa-square-o")) {
-    icon.removeClass("fa-square-o");
-    icon.addClass("fa-check-square-o");
     selectAllEmails();
   } else {
     if (icon.hasClass("fa-check-square-o")) {
-      icon.removeClass("fa-check-square-o");
-      icon.addClass("fa-square-o");
       unSelectAllEmails();
     } else {
-      icon.removeClass("fa-minus-square-o");
-      icon.addClass("fa-check-square-o");
       selectAllEmails();
     }
   }
 }
 
 var enableButtons = function() {
-  $('.db').removeAttr("disabled");
+  $('.db').prop("disabled", false);
 }
 
 var disableButtons = function() {
-  $('.db').attr("disabled", true);
+  $('.db').prop("disabled", true);
 }
 
-var selectEmailClick = function() {
+var setButtonState = function() {
   if ($('.email-row :checked').length === 0) {
     disableButtons();
   } else {
     enableButtons();
   }
+}
+
+var selectEmailClick = function() {
   $(this).closest(".email-row").toggleClass("selected");
+  setTopBarState();
 }
 
 var insertLabelMenu = function(val) {
@@ -147,12 +176,24 @@ var addLabelToMenu = function() {
   $(".modal-content input").value().insertLabelMenu(val);
 }
 
+var toggleStar = function() {
+  if ($(this).hasClass("fa-star-o")) {
+    $(this).removeClass("fa-star-o");
+    $(this).addClass("fa-star");
+  } else {
+    $(this).removeClass("fa-star");
+    $(this).addClass("fa-star-o");
+  }
+}
+
 $(document).ready(function() {
   $(':checkbox').click(selectEmailClick);
   $(".subject").click(markAsRead);
   $(".mar").click(markAsRead);
-  $(".mar").click(markAsUnRead);
-  $(".deleteEmail").click(deleteEmail);
+  $(".mau").click(markAsUnRead);
+  $(".delete-email").click(deleteEmail);
   $(".multiSelect").click(multiSelectStateChange);
   $(".modal-save").click(addLabelToMenu);
+  $(".star").click(toggleStar);
+  setMultiSelectIcon();
 });
